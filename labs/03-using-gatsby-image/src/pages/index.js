e2072c1ebefca76d4aee3c394c8966aa7a1bccd0
Mark Lee <jlee330@its.jnj.com>
2019-02-01 16:10:39 -0500

    Gatsby work

--- labs/03-using-gatsby-image/src/pages/index.js
+++ labs/03-using-gatsby-image/src/pages/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
-
-// TODO: import Bio component
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-
+import Bio from '../components/bio'
 function IndexPage({ data }) {
   return (
     <Layout>
       <SEO title="Home" keywords={[`blog`]} />
-      {/* TODO: render Bio component */}
+      <Bio/>
       <ul>
         {
           data.articles.edges.map(({ node }) => (
